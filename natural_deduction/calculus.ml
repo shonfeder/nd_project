fbe19ffa988edba641cad06ecc33d1f7e8b110b3
Shon Feder <shon.feder@gmail.com>
2019-11-14 15:54:39 -0500

    Complete calculus

--- natural_deduction/calculus.ml
+++ natural_deduction/calculus.ml
@@ -80,49 +80,87 @@ module Intro = struct
   let neg : Formula.t -> t -> t option =
     fun f fig ->
     let open Option.Monad_infix in
+    let open Formula.Infix in
     imp f fig >>= fun fig ->
     let conclusion = Figure.endformula fig in
-    if not Formula.(equal (def F) conclusion) then
-      None
-    else
-      let open Formula.Infix
-      in Option.some @@
+    Option.some_if Formula.(equal (def F) conclusion) begin
       deriv
         [fig]
         (****)
         (!! f)
+    end
 end
 
 module Elim = struct
   open Figure
-  (* open Expression *)
+  open Expression
 
   let conj_left : t -> t option =
     fun fig ->
-    match Figure.endformula fig with
-    | Comp (And (a, _)) -> Option.some @@
-      deriv
-        [fig]
-        (***)
-        a
-    | _ -> None
+    let open Option.Monad_infix in
+    Figure.endformula fig |> Formula.get_and >>| fun (a, _) ->
+    deriv
+      [fig]
+      (***)
+      a
 
   let conj_right : t -> t option =
     fun fig ->
-    match Figure.endformula fig with
-    | Comp (And (_, b)) -> Option.some @@
+    let open Option.Monad_infix in
+    Figure.endformula fig |> Formula.get_and >>| fun (_, b) ->
+    deriv
+      [fig]
+      (***)
+      b
+
+  let disj : t -> t -> t -> t option =
+    fun a_or_b_fig c_from_a_fig c_from_b_fig ->
+    let open Option.Monad_infix in
+    Figure.endformula a_or_b_fig |> Formula.get_or >>= fun (a, b) ->
+    let c_from_a = Figure.endformula c_from_a_fig
+    and c_from_b = Figure.endformula c_from_b_fig
+    in
+    Option.some_if Formula.(equal c_from_a c_from_b) c_from_a >>= fun c ->
+    Intro.imp a c_from_a_fig >>= fun _ ->
+    Intro.imp b c_from_b_fig >>| fun _ ->
+    deriv
+      [a_or_b_fig; c_from_a_fig; c_from_a_fig]
+      (**************************************)
+      c
+
+  let imp : t -> t -> t option =
+    fun a_fig a_imp_b_fig ->
+    let open Option.Monad_infix in
+    Figure.endformula a_imp_b_fig |> Formula.get_imp >>= fun (a, b) ->
+    let a' = Figure.endformula a_fig in
+    Option.some_if Formula.(equal a a') begin
       deriv
-        [fig]
-        (***)
+        [a_fig; a_imp_b_fig]
+        (******************)
         b
-    | _ -> None
+    end
 
-  (* let disj : t -> t -> t -> t option =
-   *   fun a_or_b_fig a_imp_c_fig b_imp_c_fig ->
-   *   let a_or_b = Figure.endformula a_or_b_fig
-   *   and a_imp_c = Figure.endformula a_imp_c_fig
-   *   and b_imp_c = Figure.endformula b_imp_c_fig
-   *   in *)
+  let neg : t -> t -> t option =
+    fun a_fig neg_a_fig ->
+    let open Option.Monad_infix in
+    let a = Figure.endformula a_fig
+    and neg_a = Figure.endformula neg_a_fig
+    in
+    Formula.get_not neg_a >>= fun a' ->
+    Option.some_if Formula.(equal a a') begin
+      deriv
+        [a_fig; neg_a_fig]
+        (****************)
+        Formula.(def F)
+    end
 
-
+  let absurd : t -> Formula.t -> t option =
+    fun false_fig d ->
+    let f = Figure.endformula false_fig in
+    Option.some_if Formula.(equal (def F) f) begin
+      deriv
+        [false_fig]
+        (*********)
+        d
+    end
 end
