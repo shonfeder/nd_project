5384eb7117b84d999f0f39e2e5335f679ca82f9f
Shon Feder <shon.feder@gmail.com>
2020-01-05 20:56:24 -0500

    Fix incorrect tests

--- test/tactics_test.ml
+++ test/tactics_test.ml
@@ -3,6 +3,8 @@ open Natural_deduction
 
 open Proof
 
+(** TODO Add proper tests and clean up these beasts *)
+
 let ex_proof = Option.value_exn Ex_proofs.Ex_1_1.proof
 
 let print_fig fig =
@@ -14,60 +16,64 @@ let tactics_unit_tests = Helpers.unit_suite "Proof Tactics Unit Tests"
       begin
         let p = Partial.Formula.complete Ex_proofs.Prop.x in
         (* The figure initially has a single initial formula *)
-        let fig = Notation.Figure.initial p in
+        let fig =
+          let open Notation.Figure in
+          deriv
+            [initial p]
+            ~rule:Partial.Rule.hole
+            p
+        in
         let () = print_fig fig in
         Helpers.unit "reiter reiterates initial figures in derivation"
           Alcotest.(list Nd_unit.testable_partial_formula)
           (* After reiter, it's expected to have 2 initial formula *)
           ~expected:[p; p]
           ~actual:begin
+            let zip = Proof.Zipper.of_figure fig
+                      |> Zipper.move_up
+                      |> Option.value_exn
+            in
             Proof.Tactic.apply
-              (Zipper.of_figure fig)
+              zip
               Proof.Tactic.Reiter
             |> Result.ok |> Option.value_exn
             |> (fun z -> Zipper.to_figure z |> print_fig; z)
-            |> Zipper.focus
+            |> Zipper.to_figure
             |> Notation.Figure.initial_formulae
           end
       end;
 
-      (* begin
-       *   let p = Partial.Formula.complete Ex_proofs.Prop.x in
-       *   (\* The figure initially has a single initial formula *\)
-       *   let fig = Notation.Figure.initial p in
-       *   let () = print_fig fig in
-       *   Helpers.unit "reiter reiterates initial figures in derivation"
-       *     Alcotest.(list Nd_unit.testable_partial_formula)
-       *     (\* After reiter, it's expected to have 2 initial formula *\)
-       *     ~expected:[p; p]
-       *     ~actual:begin
-       *       Proof.Tactic.apply
-       *         (Zipper.of_figure fig)
-       *         Proof.Tactic.Reiter
-       *       |> Result.ok |> Option.value_exn
-       *       |> (fun z -> Zipper.to_figure z |> print_fig; z)
-       *       |> Zipper.focus
-       *       |> Notation.Figure.initial_formulae
-       *     end
-       * end; *)
-
       begin
         let p = Partial.Formula.complete Ex_proofs.Prop.x in
         (* The figure initially has a single initial formula *)
-        let fig = Notation.Figure.initial p in
+        let fig =
+          let open Notation.Figure in
+          deriv
+            [initial p]
+            ~rule:Partial.Rule.hole
+            p
+        in
         let () = print_fig fig in
         Helpers.unit "Focused.apply_tactics reiterates initial figures in derivation"
           Alcotest.(list Nd_unit.testable_partial_formula)
           (* After reiter, it's expected to have 2 initial formula *)
           ~expected:[p; p]
           ~actual:begin
-            let focused = Proof.Focused.of_figure fig in
+            let focused =
+              let proof = Proof.Zipper.of_figure fig
+                          |> Zipper.move_up
+                          |> Option.value_exn
+              in
+              Proof.Focused.{proof; tactics = []}
+            in
             Proof.Focused.apply_tactic
               focused
               Proof.Tactic.Reiter
-            |> Result.ok |> Option.value_exn |> (fun Proof.Focused.{proof; _} -> proof)
+            |> Result.map_error ~f:Tactic.show_err
+            |> Result.ok_or_failwith
+            |> (fun Proof.Focused.{proof; _} -> proof)
             |> (fun z -> Zipper.to_figure z |> print_fig; z)
-            |> Zipper.focus
+            |> Zipper.to_figure
             |> Notation.Figure.initial_formulae
           end
       end
