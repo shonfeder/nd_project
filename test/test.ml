2226370a41aa8c6774f3d99c3e9111ff5d730801
Shon Feder <shon.feder@gmail.com>
2020-01-11 21:42:44 -0500

    Rename file

--- test/test.ml
+++ test/test.ml
@@ -4,6 +4,6 @@ let () =
   Alcotest.run "Tests"
     [ Calculus_test.Complete.unit_tests
     ; Calculus_test.Partial.unit_tests
-    ; Proof_test.zipper_unit_tests
+    ; Proof_zipper_test.zipper_unit_tests
     ; Tactics_test.tactics_unit_tests
     ];
